ed15506ab92c93e9727449486f72e726f4373d14
logan <logan.peach@gmail.com>
2023-11-08 14:22:31 -0600

    Getting started!

--- content/entities/heros.h
+++ content/entities/heros.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <memory>
+
+class Entity;
+class Engine;
+class Action;
 
 namespace Heros {
 
+    void make_knight(std::shared_ptr<Entity> entity);
 
+    std::unique_ptr<Action> behavior(Engine& engine, Entity&);
 }
