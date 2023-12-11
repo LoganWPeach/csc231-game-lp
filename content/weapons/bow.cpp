95e51130c947333f43a79426e81d67af68debacd
logan <logan.peach@gmail.com>
2023-12-11 11:55:10 -0600

    Still problems with errors.

--- content/weapons/bow.cpp
+++ content/weapons/bow.cpp
@@ -9,9 +9,11 @@ Bow::Bow(int damage)
     :Weapon{"bow_drawn", damage} {}
 
 void Bow::use(Engine& engine, Entity& attacker, Entity& defender) {
-    std::shared_ptr<Event> hit{};
-    hit->add_next(Hit(defender, damage));
+    engine.events.create_event<Hit>(defender, damage);
 
+
+
+    // hit->add_next(Hit(defender, damage));
     // Vec direction = defender.get_position() - attacker.get_position();
     // std::shared_ptr<Event> swing = engine.events.create_event<Swing>(sprite, direction);
     // std::shared_ptr<Event> lightning = std::make_shared<Lightning>(defender.get_position());
